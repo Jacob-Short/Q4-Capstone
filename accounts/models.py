a33b112c820164dfc66c8e293795d3dde025197c
Aris Fornasari <arisfornasari@Ariss-MacBook-Pro.local>
2021-10-06 13:39:50 -0700

    Modified the MyUser model/removed homepage and added bio and favoirt_game

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,15 @@
 from typing import AbstractSet
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+# from games.models import Game
 
 # Create your models here.
 class MyUser(AbstractUser):
-    homepage = models.URLField(null=True, blank=True)
     displayname = models.CharField(max_length=30, null=True, blank=True)
     email = models.EmailField()
     picture = models.ImageField(upload_to=None, max_length=100)
+    bio = models.TextField()
+    # favorite_game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
