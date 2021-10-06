d6340ae2d5378ab2e294541b62d73799209e0446
Jacob-Short <thatmanjshort@gmail.com>
2021-10-05 17:25:22 -0700

    created game model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
+from accounts.models import MyUser
 
-# Create your models here.
+# from reviews.models import Review
+
+
+class Game(models.Model):
+    POOR = 'poor'
+    GOOD = 'good'
+    GREAT = 'great'
+    EXCEPTIONAL = 'exceptional'
+
+    XBOX = 'xbox'
+    PLAYSTATION = 'playstation'
+    PC = 'pc'
+    SWITCH = 'switch'
+
+    SYS_CHOICES = [
+        (XBOX, 'xbox'),
+        (PLAYSTATION, 'playstation'),
+        (PC, 'pc'),
+        (SWITCH, 'switch'),
+    ]
+
+    RATING_CHOICES = [
+        (POOR, 'poor'),
+        (GOOD, 'good'),
+        (GREAT, 'great'),
+        (EXCEPTIONAL, 'exceptional'),
+    ]
+
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=150)
+    rating = models.CharField(max_length=150, choices=RATING_CHOICES)
+    platform = models.CharField(max_length=150, choices=SYS_CHOICES)
+    released = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+
+    # reviews = models.ForeignKey(Review, on_delete=models.CASCADE, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
