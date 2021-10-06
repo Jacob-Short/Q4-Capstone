1195c270b275599347fcf8138d8542db815cecda
Jacob-Short <thatmanjshort@gmail.com>
2021-10-06 09:46:59 -0700

    implementing view for pre populating db

--- games/views.py
+++ games/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
+import sqlite3
+import random
+
 '''
 Game:
     id, name, slug, language, games_count, image_background, 
     esbr rating, screen_shots, released at, platform
-    
+
 '''
 
 class GamesHomeView(View):
@@ -30,3 +33,29 @@ class CreateGameView(View):
     def post(self, request):
         ...
 
+
+class FillDbWithGames(View):
+    '''instantiate db with games from api'''
+
+    def get(self, request):
+        ...
+
+
+    def post(self, request):
+
+        def populate(self, reuqest):
+
+            # conn = sqlite3.connect("the-portal.db")
+
+            # c = conn.cursor()
+
+            # c.execute(
+            #     """
+            #     CREATE TABLE IF NOT EXISTS games (
+            #         ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            #         name VARCHAR(150) NOT NULL,
+            #         password VARCHAR(150) NOT NULL,
+                    
+            #     )
+            #     """
+            # )
