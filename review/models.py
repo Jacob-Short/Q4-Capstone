ac6e617ba19df36835b4b709b65730afb4add9c4
Alonaazure <Alonaazure@gmail.com>
2021-10-07 12:24:47 -0500

    review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import DateField
+from django.utils import timezone
+from accounts.models import MyUser
+# from games.model import Game
 
-# Create your models here.
+
+class Review(models.Model):
+    name = models.CharField(max_length=150)
+    text = models.TextField()
+    # game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    time_created = DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
