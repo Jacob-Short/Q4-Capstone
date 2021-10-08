4f53fb8b9abc786ec6e0bf12a7a1d1cff363ff5c
Alonaazure <Alonaazure@gmail.com>
2021-10-08 14:28:27 -0500

    reviews complete

--- review/models.py
+++ review/models.py
@@ -6,12 +6,22 @@ from games.models import Game
 
 
 class Review(models.Model):
+    CHOICES_RATING = [
+      (1, '1'),
+      (2, '2'),
+      (3, '3'),
+      (4, '4'),
+      (5, '5'),
+    ]
+    rating = models.CharField(choices=CHOICES_RATING, max_length=150)
     name = models.CharField(max_length=150)
     text = models.TextField()
     game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True, blank=True)
     user_created = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='created_by')
     time_created = DateField(default=timezone.now)
+    
 
     def __str__(self):
         return self.name
+
 
--- review/views.py
+++ review/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-
 from games.models import Game
 from review.models import Review
 from review.forms import CreateReviewForm
@@ -21,9 +20,23 @@ class CreateReview(View):
         if form.is_valid():
             data = form.cleaned_data
             review = Review.objects.create(
+                rating = data['rating'],
                 name = data['name'],
                 text = data['text'],
                 game = data['game'],
                 user_created = request.user,
             )
             return redirect('/')
+
+
+class Reviews(View):
+    """displays all reviews"""
+    def get(self, request):
+      reviews = Review.objects.all()
+      template = "reviews.html"
+      context = {'reviews':reviews}
+      return render(request, template, context)
+
+
+    def post(self, request):
+      ...
