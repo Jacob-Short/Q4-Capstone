e3a40ceb9ca286a359f498e7c8ddb35fc8b26c1a
Jacob-Short <thatmanjshort@gmail.com>
2021-10-12 10:57:27 -0700

    created views for signing in and logging in, as well as login, sign up for front end

--- client/portal/src/App.js
+++ client/portal/src/App.js
@@ -6,6 +6,9 @@ import React, { Fragment } from 'react';
 
 import { Route, Switch } from "react-router-dom";
 
+import  Login  from './views/Login'; 
+import  SignUp  from './views/SignUp'; 
+
 
 function App() {
   return (
@@ -20,6 +23,8 @@ function App() {
             <Route exact path="/login" component={Login} />
             <Route exact path="/dashboard" component={Dashboard} /> */}
             <Route exact path="/games" component={AllGames} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
             {/* <PrivateRoute exact path="/profile" component={Profile} />
             <PrivateRoute exact path="/about" component={About} /> */}
           </Switch>
--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -3,9 +3,25 @@ from rest_framework.serializers import (
     HyperlinkedModelSerializer,
 )
 
+from rest_framework.views import APIView
+
 from accounts.models import MyUser
 
 class MyUserSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = [
+            'gamer_tag',
+            'email',
+            'picture',
+            'bio',
+        ]
+
+
+class CreateMyUserSerializer(HyperlinkedIdentityField):
+    ...
+
+class CreateMyUserSerializer(APIView):
     class Meta:
         model = MyUser
         fields = [
