292137b3cd8e27f95819d0adc69058269a29a669
Jacob Short <thatmanjshort@gmail.com>
2021-10-13 20:10:31 -0700

    implemented faq-comment model

---
+++ faq_comment/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from mptt.models import MPTTModel, TreeForeignKey
+from accounts.models import MyUser
+
+from faq.models import UserFaq
+
+
+class File(MPTTModel):
+    comment = models.CharField(max_length=200, unique=True)
+    parent = TreeForeignKey(
+        UserFaq, on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+    user = models.ForeignKey(MyUser, on_delete=CASCADE, null=True, related_name='user_filed')
+
+    def __str__(self):
+        return self.comment
+
+
+    class MPTTMeta:
+        order_insertion_by = ['comment']
