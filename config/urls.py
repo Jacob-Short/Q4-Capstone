26910be3153b9da26649cf5bc18d0e9a45cbc901
Jacob Short <thatmanjshort@gmail.com>
2021-11-06 17:50:40 -0700

    updated reqs

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path 
 from django.views.static import serve
-import settings
+from django.conf import settings
 
 from accounts.urls import urlpatterns as account_url
 from message.urls import urlpatterns as message_url
